59f14ae5d6c535eff7b06791bb20999e7a0f5b62
Natalya Maximova <natka-31@mail.ru>
2022-09-30 16:23:43 +0500

    добавить авторизацию запроса, классы ошибок

--- app.js
+++ app.js
@@ -2,24 +2,30 @@ require('dotenv').config(); // Dotenv — это модуль с нулевой 
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
 const helmet = require('helmet'); // помогает защитить приложение от некоторых широко известных веб-уязвимостей путем соответствующей настройки заголовков HTTP
+const { errors } = require('celebrate');
 const mongoose = require('mongoose');
+const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const NotFoundError = require('./Error/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(cors());
 app.use(helmet());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 app.use(requestLogger); // подключаем логгер запросов
+app.use(auth); // защищает маршруты, которым нужны авторизация
 
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+  next(new NotFoundError('Запрашиваемая страница не найдена'));
+});
 
 app.use(errorLogger); // подключаем логгер ошибок
 app.use(errors()); // обработчик ошибок celebrate
