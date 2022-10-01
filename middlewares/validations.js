615e87293ee24deb38d96cd8dfb2efbb2cd8bd1e
Natalya Maximova <natka-31@mail.ru>
2022-10-01 18:12:38 +0500

    Добавить роуты и валидацию celebrate users, movies

---
+++ middlewares/validations.js
@@ -0,0 +1,44 @@
+const { celebrate, Joi } = require('celebrate');
+const isURL = require('validator/lib/isURL');
+
+const validateUrl = (value, helpers) => {
+  if (isURL(value)) {
+    return value;
+  }
+  return helpers.message('Некоректный URL-адрес');
+};
+
+const validationUpdateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().email().required(),
+  }),
+});
+
+const validationCreateMovie = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().custom(validateUrl).required(),
+    trailerLink: Joi.string().custom(validateUrl).required(),
+    thumbnail: Joi.string().custom(validateUrl).required(),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+const validationDeleteMovie = celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().length(24).hex().required(),
+  }),
+});
+
+module.exports = {
+  validationUpdateUser,
+  validationCreateMovie,
+  validationDeleteMovie,
+};
